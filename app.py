2debede6f525444aa9f531a1f4ed2ad52cd0d02a
Alberto <rinconegocio@gmail.com>
2021-11-22 07:06:51 -0300

    change folder structure

---
+++ app.py
@@ -0,0 +1,28 @@
+import logging
+
+from chalice import Chalice
+from binance.client import Client
+from chalicelib.secrets import get_binance_secret
+
+app = Chalice(app_name='wp-bot-platform')
+API_KEY, SECRET_KEY = get_binance_secret()
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+@app.route('/coin/{coin}', methods=['GET'])
+def CoinUSDTHandler(coin=None):
+
+    client = Client(API_KEY, SECRET_KEY)
+    symbol = coin + 'USDT' if coin else 'BTCUSDT'
+
+    try:
+        avg_price = client.get_avg_price(symbol=symbol)
+    except Exception as e:
+        raise e 
+    response = {
+        'price': avg_price.get('price')
+    }
+
+    return  response
---
+++ chalicelib/secrets.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+import logging
+
+from botocore.exceptions import ClientError
+
+
+def get_binance_secret():
+
+    secret_name = "wp-bot/BinanceSecrets"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        logging.error('[get_binance_secret] Something happening retrieving the secrets %s', repr(e))
+        
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret_string = get_secret_value_response['SecretString']
+        else:
+            logging.error('[get_binance_secret] No secrets string in aws response')
+            return None, None
+    
+    secret = json.loads(secret_string)
+    api_key = secret.get('BinanceApiKey')
+    secret_key = secret.get('BinanceSecretKey')
+    
+    if not api_key or not secret_key:
+        logging.error('[get_binance_secret] Impossibility to retrieve the secrets: %s', get_secret_value_response)
+
+    return api_key, secret_key
