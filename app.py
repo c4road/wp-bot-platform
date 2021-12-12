6fe4536ba8199b91cb8c9d2056f19cf1c294b7d6
Alberto <rinconegocio@gmail.com>
2021-12-12 08:50:15 -0300

    cleaning

--- app.py
+++ app.py
@@ -34,17 +34,17 @@ def WhatsappAckHandler():
     body = app.current_request.json_body
     logging.info("This is what is comming from twilio - %s", body)
 
-    account_sid = 'ACa7d17651bc98465ae590d0a6e6443a06' 
-    auth_token = '4f6fa92850cfffc32d8049df89c32bfb'
-    client = Client(account_sid, auth_token) 
+    # account_sid = '' 
+    # auth_token = ''
+    # client = Client(account_sid, auth_token) 
 
-    try:
-        message = client.messages.create( 
-                                    from_='whatsapp:+14155238886',  
-                                    body='que paso pana mio',      
-                                    to='whatsapp:+5491122520361' 
-                                ) 
-    except Exception as e:
-        logging.error("Something bad happened calling twilio - %s", e)
+    # try:
+    #     message = client.messages.create( 
+    #                                 from_='whatsapp:+14155238886',  
+    #                                 body='que paso pana mio',      
+    #                                 to='whatsapp:+5491122520361' 
+    #                             ) 
+    # except Exception as e:
+    #     logging.error("Something bad happened calling twilio - %s", e)
 
     return 
